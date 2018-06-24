f1ce54e64972bd71e5766896d33c3ec68b3d3e58
Gang Liu <462388381@qq.com>
2018-06-24 12:04:18 +0800

    Streamr_MI_Scale

---
+++ app.js
@@ -0,0 +1,44 @@
+﻿/**
+ * 小程序配置文件
+ */
+
+// 此处主机域名修改成腾讯云解决方案分配的域名
+var host = '$URL';
+
+var config = {
+
+  // 下面的地址配合云端 Demo 工作
+  service: {
+    host,
+
+    // 登录地址，用于建立会话
+    loginUrl: `${host}/weapp/login`,
+
+    // 测试的请求地址，用于测试会话
+    requestUrl: `${host}/weapp/user`,
+
+    // 测试的信道服务地址
+    tunnelUrl: `${host}/weapp/tunnel`,
+
+    // 上传图片接口
+    uploadUrl: `${host}/weapp/upload`
+  }
+};
+
+App({
+  onLaunch: function (options) {
+    // Do something initial when launch.
+  },
+  onShow: function (options) {
+    // Do something when show.
+  },
+  onHide: function () {
+    // Do something when hide.
+  },
+  onError: function (msg) {
+    console.log(msg)
+  },
+  globalData: 'I am global data'
+})
+
+module.exports = config;
